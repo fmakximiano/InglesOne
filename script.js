781846923f50c5f380a7c84d7fa0c9dca950afab
fmakximiano <fernasbae@gmail.com>
2022-07-08 15:35:31 -0300

    InglesArray

---
+++ script.js
@@ -0,0 +1,27 @@
+//Create 3 variables, each with an array:- Comedy-Action-Fantasy //
+
+let comedy = [
+    {Title:"Minha mãe é um peça",Director:"André Pellenz", Year:"2013"}]
+    console.log(comedy)
+    
+    let action = [
+    {Title:"Duro de matar 4.0",Director:"Len Wiseman", Year:"2007"}]
+    console.log(action)
+    
+    let fantasy = [
+    {Title:"O som da magina",Director:"Kim Seong-Yoon", Year:"2022"}]
+    console.log(fantasy)
+    
+   // Within each array, enter three objects, containing:title-director-year //
+    
+
+    let preferido = "sim"
+    let escolha = ""
+     
+    if (preferido == sim){
+    console.log ("My favorit movie is...")
+    } if else { 
+    console.log("Not even 5 stars...")
+    }
+    else { console.log ("Sem opção")
+}
